a2a157219595b4b53908910ddc4f4047c5bf8f4d
George Onukwulu <113475638+GeorgeNonso@users.noreply.github.com>
2023-02-16 08:47:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Write your AC Load Estimator solution here
+width = float(input("enter the room's width"))
+height = float(input("enter the rooms hieght"))
+number_of_people = int(input("enter the size of the people"))
+
+horsepower = width * height * number_of_people
+print (horsepower)
