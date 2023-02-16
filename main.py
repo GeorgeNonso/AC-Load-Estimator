4814fdb1f3304c37d07edcfd92c03dc6787882df
George Onukwulu <113475638+GeorgeNonso@users.noreply.github.com>
2023-02-16 08:53:27 +0000

    Updated

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ width = float(input("enter the room's width"))
 height = float(input("enter the rooms hieght"))
 number_of_people = int(input("enter the size of the people"))
 
-horsepower = width * height * number_of_people
+horsepower = width * height * 0.1 + number_of_people * 0.06
 print (horsepower)
